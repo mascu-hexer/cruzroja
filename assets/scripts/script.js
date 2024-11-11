cc0a51d9713558d264fe7d0fe2207baf54769e61
mmasalm734 <mmasalm734@g.educaand.es>
2024-11-11 14:50:05 +0100

    falta crear el sticky de la nav-bar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,26 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const burgerIcon = document.getElementById('burger-icon');
     const closeIcon = document.getElementById('close-icon');
 
-    burgerMenu.addEventListener('click', function() {
+    burgerMenu.addEventListener('click', function(event) {
+        if (event.target === closeIcon) {
+            return;
+        }
+
         if (navBar.classList.contains('show')) {
-            // Cerrar el menú y revertir la rotación
             navBar.classList.remove('show');
-            burgerIcon.style.transform = 'rotate(0deg)';
             closeIcon.style.display = 'none';
             burgerIcon.style.display = 'block';
-        } else {
-            // Abrir el menú y rotar el icono
+        } else {     
             navBar.classList.add('show');
-            burgerIcon.style.transform = 'rotate(180deg)'; // Rotar 180 grados
-            closeIcon.style.display = 'block'; // Mostrar el icono de cierre
-            burgerIcon.style.display = 'none'; // Ocultar el icono de hamburguesa
+            closeIcon.style.display = 'block';
+            burgerIcon.style.display = 'none';
         }
     });
 
-    closeIcon.addEventListener('click', function() {
-        // Cerrar el menú al hacer clic en el icono de cierre
+    closeIcon.addEventListener('click', function(event) {
+        event.stopPropagation();
         navBar.classList.remove('show');
-        burgerIcon.style.transform = 'rotate(0deg)';
         closeIcon.style.display = 'none';
         burgerIcon.style.display = 'block';
     });
