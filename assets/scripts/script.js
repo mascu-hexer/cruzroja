3236c05e9b1595d6dc7731d447ab3f090a06bb47
mmasalm734 <mmasalm734@g.educaand.es>
2024-11-12 12:31:00 +0100

    Enviada para Corregir

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -27,5 +27,27 @@ document.addEventListener('DOMContentLoaded', function() {
         burgerIcon.style.display = 'block';
     });
 
+    const offset = 65; // Desplazamiento adicional en píxeles
 
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            if (this.getAttribute('href') === '#') {
+                // Permitir que el enlace recargue la página
+                return;
+            }
+
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - offset;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
